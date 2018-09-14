79bd6d7f8b2891ca5744f4f25e59de62f4d9287c
Chenle <chenle.li@student.ecp.fr>
2018-09-14 18:06:11 +0200

    docker-compose added

--- docker/docker_init.sh
+++ docker/docker_init.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y whatever
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
 apt-cache policy docker-ce
+sudo apt-get install docker-compose -y whatever
